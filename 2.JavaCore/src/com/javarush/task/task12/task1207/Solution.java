91fea39f7e4eeed9a70126bcecb82d131406d669
darkpradoJava <darkpradoJava@yandex.ru>
2019-03-23 13:25:57 +0300

    12.4 done

---
+++ 2.JavaCore/src/com/javarush/task/task12/task1207/Solution.java
@@ -0,0 +1,23 @@
+package com.javarush.task.task12.task1207;
+
+/* 
+Int и Integer
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        int a = 5;
+        Integer b = 7;
+        print(a);
+        print(b);
+    }
+
+    //Напишите тут ваши методы
+    public static void print(int n) {
+        System.out.println(n);
+    }
+
+    public static void print(Integer n) {
+        System.out.println(n);
+    }
+}
---
+++ 2.JavaCore/src/com/javarush/task/task12/task1208/Solution.java
@@ -0,0 +1,32 @@
+package com.javarush.task.task12.task1208;
+
+/* 
+Свобода печати
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    //Напишите тут ваши методы
+    public static void print(int n) {
+        System.out.println(n);
+    }
+
+    public static void print(String n) {
+        System.out.println(n);
+    }
+
+    public static void print(float n) {
+        System.out.println(n);
+    }
+
+    public static void print(Double n) {
+        System.out.println(n);
+    }
+
+    public static void print(Integer n) {
+        System.out.println(n);
+    }
+}
